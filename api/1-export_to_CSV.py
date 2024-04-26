2f832016b131eea50e3d1e2215ec965936b51201
elnurra <elnurra@code.edu.az>
2024-04-26 12:39:36 +0400

    bug-fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-'''
-module that uses REST api to return info on user's TODO and write to file
-'''
-
+"""Gather data from an API"""
+import csv
+import json
 import requests
 import sys
 
 
-def todocsv():
-    ''' function that gets todo tasks and exports data to csv file '''
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(sys.argv[1]))
-    new = r.json()
-    name = new.get('username')
-    userid = new.get('id')
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                     .format(sys.argv[1]))
-    new = r.json()
-    size = len(new)
-    stat = []
-    titles = []
-    for i in range(0, size):
-        stat.append(new[i].get('completed'))
-        titles.append(new[i].get('title'))
-    with open("{}.csv".format(userid), 'w') as f:
-        for i in range(0, size):
-            f.write('"{}","{}","{}","{}"\n'.format(userid, name,
-                                                   stat[i], titles[i]))
-
-
 if __name__ == "__main__":
-    todocsv()
+    if len(sys.argv) == 2:
+        res = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                           f'{sys.argv[1]}/todos')
+
+        response_usr_todo = json.loads(res.text)
+        todo_all_len = len(response_usr_todo)
+        user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                            f'{sys.argv[1]}')
+        user_response = json.loads(user.text)
+        user_name = user_response["username"]
+        with open(f"{sys.argv[1]}.csv", "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+            for todo in response_usr_todo:
+                csv_list = [str(todo['userId']), str(user_name),
+                            str(todo['completed']), str(todo['title'])]
+                writer.writerow(csv_list)
