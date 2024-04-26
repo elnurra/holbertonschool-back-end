f40ab682eec01885a10ea0f1e6466b5acf5bcd64
elnurra <elnurra@code.edu.az>
2024-04-26 12:19:06 +0400

    bug-fix:fixing file api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""
-place holder
-"""
+'''
+module that uses REST api to return info on user's TODO
+'''
+
+import requests
+import sys
+
+
+def todofunc():
+    ''' function that gets todo tasks and prints the completed tasks '''
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(sys.argv[1]))
+    new = r.json()
+    name = new.get('name')
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(sys.argv[1]))
+    new = r.json()
+    size = len(new)
+    titles = []
+    count = 0
+    for i in range(0, size):
+        if new[i].get('completed'):
+            count += 1
+            titles.append(new[i].get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".format(name, count, size))
+    for i in range(0, count):
+                print("\t {}".format(titles[i]))
 
 
 if __name__ == "__main__":
-
-    import requests
-    from sys import argv
-    if len(argv) < 2:
-        exit()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
-        .format(argv[1]))
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}"
-        .format(argv[1]))
-    name = name.json()
-    name = name[0]["name"]
-    todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    todo = todo.json()
-    todo = len(todo)
-    todos = todos.json()
-    todo_list = []
-
-    for x in todos:
-        todo_list.append("\t {}".format(x["title"]))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(todos), todo))
-    for y in todo_list:
-        print(y)
+    todofunc()
