f13781e62f75cce408aeb9a2d878c40167776039
elnurra <elnurra@code.edu.az>
2024-04-26 12:17:12 +0400

    bug-fix: fixing data files

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-"""Fetches and displays an employee's to-do list from an API."""
-import requests
-import sys
-
-
-def fetch_employee_todo_list(employee_id):
-    """Fetches and displays an employee's to-do list."""
-
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
-
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-
-        EMPLOYEE_NAME = user_data["name"]
-        total_tasks = len(todos_data)
-        done_tasks = sum(1 for task in todos_data if task["completed"])
-
-        print("Employee {} is done with tasks ({}/{}):".format(
-            EMPLOYEE_NAME, done_tasks, total_tasks))
-        for task in todos_data:
-            if task["completed"]:
-                print(f"\t{task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
+"""
+place holder
+"""
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    fetch_employee_todo_list(employee_id)
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
+
+    for x in todos:
+        todo_list.append("\t {}".format(x["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for y in todo_list:
+        print(y)
